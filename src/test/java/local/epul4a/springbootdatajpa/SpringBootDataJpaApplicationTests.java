144e43b754a3a3036b8eb0c74a11826c784b1ce7
nichihachi <nichi.hc@proton.me>
2025-11-21 08:09:46 +0100

    Initial commit

---
+++ src/test/java/local/epul4a/springbootdatajpa/SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package local.epul4a.springbootdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
