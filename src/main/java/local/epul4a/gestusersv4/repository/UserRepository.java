b623c1bf1df595666e89cd2539ba3e8aa7b7b2bf
eymeric <hatchchien@protonmail.com>
2025-12-01 17:06:02 +0100

    clear before projet final

--- src/main/java/local/epul4a/gestusersv4/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package local.epul4a.gestusersv4.repository;
-
-import local.epul4a.gestusersv4.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
